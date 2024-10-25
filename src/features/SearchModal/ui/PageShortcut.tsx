7dbf7bfeffb36d8165e4111f5bbad89d695a8827
iamfiro <appturbo102@gmail.com>
2024-10-25 22:14:22 +0900

    feat: PageShortcut.tsx 추가

---
+++ src/features/SearchModal/ui/PageShortcut.tsx
@@ -0,0 +1,20 @@
+import style from './style.module.scss';
+import Link from "next/link";
+import {ReactNode} from "react";
+
+interface PageShortcutProps {
+    name: string;
+    icon: ReactNode;
+    href: string;
+}
+
+const PageShortcut = ({name, icon, href}: PageShortcutProps) => {
+    return (
+        <Link href={href} className={style.container}>
+            {icon}
+            <span className={style.name}>{name}</span>
+        </Link>
+    )
+}
+
+export default PageShortcut;
