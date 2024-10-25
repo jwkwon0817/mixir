459ff5683e5d04b5f3766860dce4fa9c966f1b99
iamfiro <appturbo102@gmail.com>
2024-10-25 23:11:12 +0900

    feat: SearchBar 클릭과 키보드 '/' 키를 눌렀을때 모달 띄우게 설정

--- src/components/molecules/SearchBar/index.tsx
+++ src/components/molecules/SearchBar/index.tsx
@@ -1,9 +1,29 @@
+'use client'
+
 import style from './style.module.scss';
 import { IoSearch } from "react-icons/io5";
+import {useModal} from "@/shared/states/useModal";
+import {useEffect} from "react";
 
 const SearchBar = () => {
+    const {openModal} = useModal();
+
+    // View의 onClick 이벤트 함수를 변수로 분리
+    const onClick = () => openModal('SearchModal');
+
+    useEffect(() => {
+        if(window) {
+            // 사용자가 '/' 키를 누르면 검색 모달을 열도록 이벤트 리스너 추가
+            window.addEventListener('keydown', (e) => {
+                if(e.key === '/') {
+                    openModal('SearchModal');
+                }
+            });
+        }
+    }, [])
+
     return (
-        <div className={style.container}>
+        <div className={style.container} onClick={onClick}>
             <IoSearch className={style.icon}/>
             <div className={style.keyboardKey}>
                 /
