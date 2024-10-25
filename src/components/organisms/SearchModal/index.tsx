68833fa0020b2aa0ede79a324be6285efa38d2fa
iamfiro <appturbo102@gmail.com>
2024-10-25 21:37:28 +0900

    feat: SearchModal 추가

---
+++ src/components/organisms/SearchModal/index.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import Modal from "@/components/molecules/Modal";
+import style from './style.module.scss';
+import {IoSearch} from "react-icons/io5";
+import {useEffect, useRef} from "react";
+
+const SearchModal = () => {
+    const InputRef = useRef<HTMLInputElement | null>(null);
+
+    useEffect(() => {
+        InputRef.current?.focus();
+    }, []);
+
+    return (
+        <Modal
+            modalId={'SearchModal'}
+            style={{
+                // SearchBar 위치에 맞게 조정
+                top: 10,
+                transform: 'translateX(-50%)',
+                borderRadius: 14,
+            }}
+        >
+            <div className={style.searchBar}>
+                <IoSearch className={style.icon}/>
+                <input
+                    ref={InputRef}
+                    className={style.input}
+                    placeholder={'검색어를 입력해주세요'}
+                />
+            </div>
+            asd
+        </Modal>
+    );
+}
+
+export default SearchModal;
