fbfc70013f6363e3634933177f00a69a348d8135
iamfiro <appturbo102@gmail.com>
2024-10-25 21:46:20 +0900

    refactor: 루트 페이지 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-'use client';
-
-import useDevice from '@/hooks/useDevice';
-import mobile from './mobile.module.scss';
-import { GoogleLoginButton } from '@/components/molecules';
+import Header from "@/components/molecules/Header";
+import SearchModal from "@/components/organisms/SearchModal";
 
 export default function Home() {
-	const { isMobile, isDesktop } = useDevice();
-
-	if (isMobile) {
-		return (
-			<div className={ mobile.container }>
-				test
-				<div>
-					1234
-				</div>
-				<GoogleLoginButton />
-			</div>
-		);
-	}
-
 	return (
-		<>desk</>
+		<>
+			<Header/>
+			<SearchModal />
+		</>
 	);
 }
