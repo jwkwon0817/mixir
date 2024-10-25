518bac032a7eb650659cea4eb25e08e8a667e7b1
iamfiro <appturbo102@gmail.com>
2024-10-26 01:33:55 +0900

    refactor: SearchModal을 Header 컴포넌트 내부로 이동

--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 import Logo from '@/../public/logo.svg';
 import SearchBar from "@/components/molecules/SearchBar";
 import Link from "next/link";
+import SearchModal from "@/components/organisms/SearchModal";
 
 
 const Header = () => {
     return (
-        <header className={style.header}>
-            <Image src={Logo} alt={'서비스 로고'} className={style.logo} />
-            <SearchBar />
-            <Link href={'/developers'} className={style.developers}>개발자</Link>
-        </header>
+        <>
+            <header className={style.header}>
+                <Image src={Logo} alt={'서비스 로고'} className={style.logo}/>
+                <SearchBar/>
+                <Link href={'/developers'} className={style.developers}>개발자</Link>
+            </header>
+            <SearchModal/>
+        </>
     )
 }
 
