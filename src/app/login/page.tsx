84b339742dc49293df41ef3a807ec84c010cdb7b
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-10-25 21:47:15 +0900

    remove page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,18 +3,20 @@
 import useDevice from '@/hooks/useDevice';
 import mobile from './mobile.module.scss';
 import { GoogleLoginButton } from '@/components/molecules';
+import Image from 'next/image';
+import logo from '@/assets/logos/logo.svg';
 
 export default function Home() {
-	const { isMobile, isDesktop } = useDevice();
+	const { isMobile } = useDevice();
 
 	if (isMobile) {
 		return (
 			<div className={ mobile.container }>
-				test
-				<div>
-					1234
+				<div className={ mobile.logoContainer }>
+					<Image src={ logo } alt={ 'Logo' } />
+					<p>선린 팀빌딩 Mixir</p>
 				</div>
-				<GoogleLoginButton />
+				<GoogleLoginButton className={ mobile.googleLoginButton } />
 			</div>
 		);
 	}
