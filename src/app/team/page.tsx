554079c575153ccbc212f6ebb5d441994f386305
iamfiro <appturbo102@gmail.com>
2024-11-19 20:55:43 +0900

    team 페이지 타이틀 부분에 팀 생성 버튼 추가

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -2,13 +2,19 @@ import {Header} from "@/components/molecules";
 import MaxWidth from "@/components/atoms/MaxWidth";
 import TeamCard from "@/components/molecules/TeamCard";
 import styles from '@/styles/app/team/page.module.scss';
+import Button from "@/components/atoms/Button";
+import {ButtonSize, ButtonStyle} from "@/shared/types/button";
+import {IoMdAdd} from "react-icons/io";
 
 const PageTeam = () => {
     return (
         <>
             <Header />
             <MaxWidth>
-                <h1>생성된 팀 빌딩</h1>
+                <div className={styles.titleContainer}>
+                    <h1>팀 빌딩</h1>
+                    <Button style={ButtonStyle.Primary} size={ButtonSize.Medium} icon={<IoMdAdd />}>팀 생성</Button>
+                </div>
                 <section className={styles.list}>
                     <TeamCard name={'수수수은수노바'} createAt={new Date()} peopleCount={32} />
                     <TeamCard name={'한녀찬'} createAt={new Date()} peopleCount={32} />
