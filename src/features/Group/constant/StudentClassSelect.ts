573a31c34b4ece6cf9a9325bcf4dbd60ff00d680
iamfiro <appturbo102@gmail.com>
2024-11-20 00:23:07 +0900

    학년, 반을 선택하기 위한 Select의 options 값을 상수화 함

---
+++ src/features/Group/constant/StudentClassSelect.ts
@@ -0,0 +1,20 @@
+export const STUDENT_GRADE_SELECT = [
+    {value: '1', label: '1학년'},
+    {value: '2', label: '2학년'},
+    {value: '3', label: '3학년'},
+]
+
+export const STUDENT_CLASS_SELECT = [
+    {value: '1', label: '1반'},
+    {value: '2', label: '2반'},
+    {value: '3', label: '3반'},
+    {value: '4', label: '4반'},
+    {value: '5', label: '5반'},
+    {value: '6', label: '6반'},
+    {value: '7', label: '7반'},
+    {value: '8', label: '8반'},
+    {value: '9', label: '9반'},
+    {value: '10', label: '10반'},
+    {value: '11', label: '11반'},
+    {value: '12', label: '12반'},
+]
