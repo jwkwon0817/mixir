72f071d12c75e11d982ee1ac9c304b88bce28210
iamfiro <appturbo102@gmail.com>
2024-11-19 22:30:19 +0900

    버튼 컴포넌트 onClick props 추가

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -8,12 +8,13 @@ interface ButtonProps {
     children?: React.ReactNode;
     isPending?: boolean;
     icon?: React.ReactNode;
+    onClick?: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({style, size, children, icon}: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({style, size, children, icon, onClick}: ButtonProps) => {
     const clonedIcon = icon ? cloneElement(icon as React.ReactElement, {className: styles.icon}) : null;
     return (
-        <button className={styles.button} data-style={style} data-size={size}>
+        <button className={styles.button} onClick={onClick} data-style={style} data-size={size}>
             {clonedIcon} {children}
         </button>
     )
